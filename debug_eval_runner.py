be2c82de63b2946fe4f0d552909595e5a763924a
HangDiep <hangdiep1010@gmail.com>
2026-05-28 21:59:55 +0700

    xoá venv_venv

--- debug_eval_runner.py
+++
@@ -1,9 +0,0 @@
-import runpy
-import traceback
-
-try:
-    runpy.run_path('src/evaluate.py', run_name='__main__')
-except Exception:
-    with open('eval_traceback.txt', 'w', encoding='utf-8') as f:
-        traceback.print_exc(file=f)
-    raise
